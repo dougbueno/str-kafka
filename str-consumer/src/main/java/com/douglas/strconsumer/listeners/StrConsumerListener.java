19fee07f8191093738a08c8faadd16828c2e6e65
dougbueno <douglas.bueno.oliveira@gmail.com>
2023-07-03 10:33:52 -0300

    Error Handler

--- str-consumer/src/main/java/com/douglas/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/douglas/strconsumer/listeners/StrConsumerListener.java
@@ -1,26 +1,30 @@
 package com.douglas.strconsumer.listeners;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.douglas.strconsumer.custom.StrConsumerCustomListener;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
+	@SneakyThrows
 	@StrConsumerCustomListener(groupId = "group-1")
 	public void create(String message) {
 		log.info("CREATE ::: Receive message {}", message);
+		throw new IllegalAccessException("EXCEPTION ...");
 	}
 
-	@StrConsumerCustomListener(groupId = "group-1")
-	public void log(String message) {
-		log.info("LOG ::: Receive message {}", message);
-	}
+//	@StrConsumerCustomListener(groupId = "group-1")
+//	public void log(String message) {
+//		log.info("LOG ::: Receive message {}", message);
+//	}
 
-	@StrConsumerCustomListener(groupId = "group-2")
+	@KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
 	public void history(String message) {
 		log.info("HISTORY ::: Receive message {}", message);
 	}
