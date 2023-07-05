4e5e08f67af83a28f8b4926f6bf8a72335ba11f6
dougbueno <douglas.bueno.oliveira@gmail.com>
2023-07-05 11:13:40 -0300

    add producer config

--- payment-service/src/main/java/com/douglas/paymentservice/model/Payment.java
+++ payment-service/src/main/java/com/douglas/paymentservice/model/Payment.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 public class Payment implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
 	private Long id;
 	private Long idUser;
 	private Long idProduct;
--- payment-service/src/main/java/com/douglas/paymentservice/resource/impl/PaymentResourceImpl.java
+++ payment-service/src/main/java/com/douglas/paymentservice/resource/impl/PaymentResourceImpl.java
@@ -2,6 +2,7 @@ package com.douglas.paymentservice.resource.impl;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class PaymentResourceImpl implements PaymentResource {
 	private final PaymentService service;
 
 	@Override
-	public ResponseEntity<Payment> payment(Payment payment) {
+	public ResponseEntity<Payment> payment(@RequestBody Payment payment) {
 		service.sendPayment(payment);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
